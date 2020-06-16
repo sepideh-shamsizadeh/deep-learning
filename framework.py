9a85b33b3679c638f0baf158713f5cc1ed40b43a
Sepideh Shamsizadeh <sepideh.shamsizadeh@gmail.com>
2020-06-16 11:06:52 +0200

    adding gitignore and optimizing functions

--- framework.py
+++ framework.py
@@ -38,6 +38,48 @@ def softmax(z):
     return t / np.sum(t)
 
 
+def cost(A, Y):
+    return (-1 / len(Y)) * np.sum(np.multiply(np.log(A), Y) + np.multiply(np.log(1 - A), (1 - Y)))
+
+
+def normalizing(X):
+    mua = (1 / X.shape[1]) * np.sum(X)
+    X = X - mua
+    sigma = (1 / X.shape[1]) * np.sum(X ** 2)
+    X /= sigma
+    return X, mua, sigma
+
+
+def initialization(parameters, activation_function):
+    L = parameters['layer_numbers']
+    for l in range(L):
+        if activation_function == 'ReLU':
+            parameters['W' + str(l + 1)] = np.random.randn() * np.squrt(2 / (parameters['n' + str(l)]))
+        else:
+            parameters['W' + str(l + 1)] = np.random.randn() * np.squrt(1 / (parameters['n' + str(l)]))
+    return parameters
+
+
+def gradient_descent(m, dZ, A):
+    dw = (1 / m) * dZ * A.T
+    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+    return dw, db
+
+
+def gradient_descent_momentum(vdw, vdb, beta, dw, db):
+    vdw = beta * vdw + dw
+    vdb = beta * vdb + db
+    return vdw, vdb
+
+
+def RMSprop(sdw, sdb, beta, dw, db):
+    sdw = beta*sdw + (1-beta)*(dw**2)
+    sdb = beta * sdb + (1 - beta) * (db ** 2)
+    return sdw, sdb
+
+
+
+
 def forward(parameters, activation_function, last_layer_activation):
     cache = {}
     L = parameters['layer_numbers']
@@ -65,6 +107,7 @@ def backward(cache, activation_function, parameters, optimization):
     cache['dZL'] = cache['AL'] - parameters['Y']
     cache['dWL'] = (1 / m) * cache['dZL'] * cache['A' + str(L - 1)].T
     cache['dbL'] = (1 / m) * np.sum(cache['dZL'], axis=1, keepdims=True)
+    Vdw, Vdb, Sdw, Sdb = 0, 0, 0, 0
     for l in reversed(range(1, L)):
         if activation_function == 'sigmoid':
             cache['dZ' + str(l)] = cache['dW' + str(l + 1)].T * cache['dZ' + str(l + 1)] * derivation_of_sigmoid(
@@ -78,47 +121,17 @@ def backward(cache, activation_function, parameters, optimization):
         elif activation_function == 'leaky_ReLU':
             cache['dZ' + str(l)] = cache['dW' + str(l + 1)].T * cache['dZ' + str(l + 1)] * derivation_of_leaky_ReLU(
                 cache['Z' + str(l)])
-        if optimization == 'gradient_descent':
-            cache['dW' + str(l)], cache['db' + str(l)] = gradient_descent(
-                l, m, cache['dZ' + str(l)], cache['A' + str(l - 1)]
+        dW, db = gradient_descent(m, cache['dZ' + str(l)], cache['A' + str(l - 1)])
+        cache['dW' + str(l)], cache['db' + str(l)] = dW, db
+        if optimization == 'gradient_descent_momentum':
+            Vdw, Vdb = gradient_descent_momentum(
+              Vdw, Vdb, parameters['beta'], dW, db
             )
-            # elif optimization == 'gradient_descent_momentum':
-            # cache['dW' + str(l)], cache['db' + str(l)]  = gradient_descent_momentum(
-            #   l, m, cache['dZ' + str(l)], cache['A' + str(l-1)]
-            # )
-
+            cache['dW' + str(l)], cache['db' + str(l)] = Vdw, Vdb
+        elif optimization == 'RMSprop':
+            Sdw, Sdb = RMSprop(
+                Sdw, Sdb, parameters['beta'], dW, db
+            )
+            cache['dW' + str(l)], cache['db' + str(l)] = dW/ (np.sqrt(Sdw + 10 ** -8)), Sdb / (np.sqrt(Sdb + 10 ** -8))
     return cache
 
-
-def cost(A, Y):
-    return (-1 / len(Y)) * np.sum(np.multiply(np.log(A), Y) + np.multiply(np.log(1 - A), (1 - Y)))
-
-
-def normalizing(X):
-    mua = (1 / X.shape[1]) * np.sum(X)
-    X = X - mua
-    sigma = (1 / X.shape[1]) * np.sum(X ** 2)
-    X /= sigma
-    return X, mua, sigma
-
-
-def initialization(parameters, activation_function):
-    L = parameters['layer_numbers']
-    for l in range(L):
-        if activation_function == 'ReLU':
-            parameters['W' + str(l + 1)] = np.random.randn() * np.squrt(2 / (parameters['n' + str(l)]))
-        else:
-            parameters['W' + str(l + 1)] = np.random.randn() * np.squrt(1 / (parameters['n' + str(l)]))
-    return parameters
-
-
-def gradient_descent(l, m, dZ, A):
-    dw = (1 / m) * dZ * A.T
-    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-    return dw, db
-
-
-def gradient_descent_momentum(vdw, vdb, beta, dw, db):
-    vdw = beta * vdw + dw
-    vdb = beta * vdb + db
-    return vdw, vdb
