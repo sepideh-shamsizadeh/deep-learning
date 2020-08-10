e1f3db0f52f84368e312080c8700921f3fe3f647
Sepideh Shamsizadeh <sepideh.shamsizadeh@gmail.com>
2020-08-10 10:09:17 +0200

    convolutional neural network

---
+++ Convolutional.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def zero_pad(X, pad):
+    return np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), mode='constant', constant_values=(0, 0))
+
+
+def conv_single_step(a_slice_prev, W, b):
+    return np.sum(a_slice_prev*W)+b
--- two_layer_neural_network.py
+++ two_layer_neural_network.py
@@ -12,12 +12,13 @@ train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_datas
 m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[1]
-#
+
 # For convenience, you should now reshape images of shape (num_px, num_px, 3) in a numpy-array of shape (num_px  ∗∗
 # num_px  ∗∗  3, 1). After this, our training (and test) dataset is a numpy-array where each column represents a
 # flattened image. There should be m_train (respectively m_test) columns.
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
 # To represent color images, the red, green and blue channels (RGB) must be specified for each pixel, and so the
 # pixel value is actually a vector of three numbers ranging from 0 to 255.
 train_set_x = train_set_x_flatten / 255.
